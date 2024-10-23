045e52806392740b551a70bbd718af66c4af6209
mohammadak95 <158298916+mohammadak95@users.noreply.github.com>
2024-10-23 19:22:15 +0300

    mega src clean

--- src/utils/formatNumber.js
+++
@@ -1,9 +0,0 @@
-// src/utils/formatNumber.js
-
-export const formatNumber = (number) => {
-    if (typeof number === 'number') {
-      return number.toFixed(2);
-    }
-    return number;
-  };
-  
